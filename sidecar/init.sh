5c7798e75a8f6af3b56305094f1010f21c695caa
Kohei Morita <moritakouhei@graffer.jp>
2023-08-08 08:51:45 +0900

    chore: add todo
    Signed-off-by: Kohei Morita <moritakouhei@graffer.jp>

--- sidecar/init.sh
+++ sidecar/init.sh
@@ -8,4 +8,6 @@
 #   [service] ---> payments:7000
 # 新:
 #   [service] ---> sidecar-proxy:8080
-iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 7000 -j REDIRECT --to-port 8080
+#
+# TODO: XXXX に適切なポート番号を設定してください。--dport で「どこ宛にきた通信を」--to-port で「どこに転送するか」を指定します。
+iptables -t nat -A PREROUTING -p tcp -i eth0 --dport XXXX -j REDIRECT --to-port XXXX
